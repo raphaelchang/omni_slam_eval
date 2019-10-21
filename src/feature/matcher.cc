18a0a138c758774934dc4c643f5612aea2d17bf3
Raphael Chang <raphaelchang@mit.edu>
2019-10-20 23:34:27 -0400

    fix lucid

--- src/feature/matcher.cc
+++ src/feature/matcher.cc
@@ -8,11 +8,11 @@ namespace feature
 Matcher::Matcher(std::string descriptor_type, double max_dist)
     : maxDistance_(max_dist)
 {
-    if (descriptor_type == "SIFT" || descriptor_type == "SURF" || descriptor_type == "KAZE" || descriptor_type == "DAISY" || descriptor_type == "LUCID" || descriptor_type == "VGG")
+    if (descriptor_type == "SIFT" || descriptor_type == "SURF" || descriptor_type == "KAZE" || descriptor_type == "DAISY" || descriptor_type == "VGG")
     {
         matcher_ = cv::BFMatcher::create(cv::NORM_L2, true);
     }
-    else if (descriptor_type == "ORB" || descriptor_type == "BRISK" || descriptor_type == "AKAZE" || descriptor_type == "FREAK" || descriptor_type == "LATCH" || descriptor_type == "BOOST")
+    else if (descriptor_type == "ORB" || descriptor_type == "BRISK" || descriptor_type == "AKAZE" || descriptor_type == "FREAK" || descriptor_type == "LATCH" || descriptor_type == "LUCID" || descriptor_type == "BOOST")
     {
         matcher_ = cv::BFMatcher::create(cv::NORM_HAMMING, true);
     }
